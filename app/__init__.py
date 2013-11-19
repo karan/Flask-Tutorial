b2945408cd0ef10aacf0b4dd1b3d26d8bf9f7f89
Karan Goel <karan@goel.im>
2013-11-19 11:11:08 -0800

    First working template

--- app/__init__.py
+++ app/__init__.py
@@ -5,4 +5,3 @@ from flask import Flask
 app = Flask(__name__) # create the application object
 
 from app import views # views are handlers that manage requests
-
--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
+from flask import render_template
 from app import app
 
 @app.route('/') # these decorators route a URL to this function
 @app.route('/index')
 def index():
-    return 'Hello World'
+    user = {'nickname': 'Karan'} # fake user
+    return render_template('index.html', title='Home', user=user)
