8f85bb812e68ebab5ae580170bfa7ba4f7441caa
Karan Goel <karan@goel.im>
2013-11-22 14:48:19 -0800

    Add User model

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
 from flask import Flask
+from flask.ext.sqlachemy import SQLAchemy
 
 app = Flask(__name__) # create the application object
 app.config.from_object('config')
+db = SQLAchemy(app)
 
-from app import views # views are handlers that manage requests
+# views are handlers that manage requests
+from app import views, models
--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 CSRF_ENABLED = True
 SECRET_KEY = 'tis-my-key'
 
@@ -9,3 +13,9 @@ OPENID_PROVIDERS = [
     { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
     { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
     { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
+
+
+# required by flask, path to the database file
+SQLACHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+# folder to store migrate data files
+SQLACHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
