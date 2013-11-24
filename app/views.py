ba80cf7a29ddab5805163eab1b1af87a8a3730a4
Karan Goel <karan@goel.im>
2013-11-23 22:53:40 -0800

    custom error messages

--- app/views.py
+++ app/views.py
@@ -108,3 +108,12 @@ def user(nickname):
     return render_template('user.html',
                            user=user,
                            posts=posts)
+
+@app.errorhandler(404)
+def internal_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
