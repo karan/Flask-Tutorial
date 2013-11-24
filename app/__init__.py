d152f285da277511c38367b9b6edbf4f3c858ef2
Karan Goel <karan@goel.im>
2013-11-23 18:06:46 -0800

    login works

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,17 @@
-#!/usr/bin/env python
-
 import os
-
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
 from config import basedir
 
-app = Flask(__name__) # create the application object
+app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
-
 lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
-# views are handlers that manage requests
 from app import views, models
+
--- app/views.py
+++ app/views.py
@@ -1,26 +1,36 @@
-#!/usr/bin/env python
-
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, db, lm, oid
 from forms import LoginForm
 from models import User, ROLE_USER, ROLE_ADMIN
 
-@app.route('/') # these decorators route a URL to this function
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+@app.before_request
+def before_request():
+    g.user = current_user
+    
+@app.route('/')
 @app.route('/index')
 @login_required
 def index():
     user = g.user
     posts = [
-        {'author': {'nickname': 'Bill Clinton'},
-         'body': 'Binder full of women!'
+        { 
+            'author': { 'nickname': 'John' }, 
+            'body': 'Beautiful day in Portland!' 
         },
-        {'author': {'nickname': 'Who?'},
-         'body': 'There goes my baby...'
+        { 
+            'author': { 'nickname': 'Susan' }, 
+            'body': 'The Avengers movie was so cool!' 
         }
     ]
-    return render_template('index.html', title='Home',
-                           user=user, posts=posts)
+    return render_template('index.html',
+        title = 'Home',
+        user = user,
+        posts = posts)
 
 @app.route('/login', methods = ['GET', 'POST'])
 @oid.loginhandler
@@ -36,33 +46,25 @@ def login():
         form = form,
         providers = app.config['OPENID_PROVIDERS'])
 
-@app.before_request
-def before_request():
-    g.user = current_user
-
 @oid.after_login
 def after_login(resp):
-    if resp.email is None or resp.email == '':
+    if resp.email is None or resp.email == "":
         flash('Invalid login. Please try again.')
         return redirect(url_for('login'))
-    user = User.query.filter_by(email=resp.email).first()
+    user = User.query.filter_by(email = resp.email).first()
     if user is None:
         nickname = resp.nickname
-        if nickname is None or nickname == '':
+        if nickname is None or nickname == "":
             nickname = resp.email.split('@')[0]
-        user = User(nickname=nickname, email=resp.email, role=ROLE_USER)
+        user = User(nickname = nickname, email = resp.email, role = ROLE_USER)
         db.session.add(user)
-        db.session.commit
+        db.session.commit()
     remember_me = False
     if 'remember_me' in session:
         remember_me = session['remember_me']
         session.pop('remember_me', None)
-    login_user(user, remember=remember_me)
-    return redirect(request.args.get('next') or url_for('index'))    
-
-@lm.user_loader
-def load_user(id):
-    return User.query.get(int(id))
+    login_user(user, remember = remember_me)
+    return redirect(request.args.get('next') or url_for('index'))
 
 @app.route('/logout')
 def logout():
